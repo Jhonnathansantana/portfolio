8f1fd3a5cbbee4ac6d27593f9146db63ece797a3
Jhonnathansantana <49243156+Jhonnathansantana@users.noreply.github.com>
2020-07-10 16:46:51 -0400

    btnbarmenu

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function openav(){
+    const element = document.querySelector("#navbar");
+    element.classList.add("open-menu");
+};
+
+function closenav(){
+    var ex = document.getElementById("navbar");
+    ex.classList.remove("open-menu");
+};
+
