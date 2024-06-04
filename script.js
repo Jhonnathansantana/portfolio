c0bf9f2fcef060ad61d95527c9d59910d2d8f323
Jhonnathan Santana <jhonnathansantana@outlook.com>
2024-06-04 08:43:55 -0400

    script

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ ScrollReveal().reveal('.section' , {delay: 500});
     //exp.style.add()
     alert(v);
 };*/
+
+
+import { faro } from '@grafana/faro-web-sdk';
+
+// ...
+// initialize faro as shown in the previous examples
+// ...
+
+// will be captured automatically
+console.info('hello world');
+
+// push log explicitly
+faro.api.pushLog(['hello world']);
+
+// will be captured automatically
+throw new Error('oh no');
+
+// push error manually
+faro.api.pushError(new Error('oh no'));
+
+// push a RUM event
+faro.api.pushEvent('click_sign_up_button')
